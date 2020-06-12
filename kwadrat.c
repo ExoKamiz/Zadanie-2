edffd75e8e4418053cbb2033e1ea2a23903f2796
Maksym <mzimak18@gmail.com>
2020-06-12 16:10:35 +0200

    Zd2

---
+++ kwadrat.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void kwadrat() {
+    int a;
+    printf("Podaj strone kwadratu: ");
+    scanf("%d", &a);
+    printf("Pole kwadratu jest: %d \n", a*a);
+}
---
+++ szescian.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void szescian() {
+    int a;
+    printf("Podaj strone szescianu: ");
+    scanf("%d", &a);
+    printf("Objetosc szescianu jest: %d \n", a*a*a);
+}
